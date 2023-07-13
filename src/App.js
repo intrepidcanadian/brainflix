8a9c846cd2e79afab5f958d4eb82ac655272ffab
intrepid <tony.kp.lau@gmail.com>
2023-07-13 00:17:11 -0400

    comment debugging

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/component';
 import Home from './pages/page';
 import VideoUpload  from './pages/videoupload.js' 
 
 
-const API_KEY = '082e3755-9d57-450b-bc71-ba4741e1a6d4'; 
+// no longer needed
+// const API_KEY = '082e3755-9d57-450b-bc71-ba4741e1a6d4'; 
+const API_URL = 'http://localhost:8000/videos';
 
 function App() {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState([]);
+
+  // i pass down videos from fetching from own api
+  const [videos, setVideos] = useState([]);
+
+  // this is for initial mounting of videos
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
+  const fetchVideos = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      // i set videos to the data from api
+      setVideos(data);
+    } catch (error) {
+      console.error('Error fetching videos:', error);
+    }
+  };
 
   const handleVideoSelection = (videoId) => {
     setSelected(videoId);
@@ -19,10 +40,10 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home apiKey={API_KEY} />} />
+          <Route path="/" element={<Home videos={videos} />} />
           <Route path="/upload" element={<VideoUpload />} />
           <Route path="/video/:id"
-            element={<Home apiKey={API_KEY} selected={selected} setSelected={handleVideoSelection} />}
+            element={<Home videos={videos} selected={selected} setSelected={handleVideoSelection} />}
           />
         </Routes>
       </BrowserRouter>
